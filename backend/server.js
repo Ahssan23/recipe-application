fef34876cc74bd4bd6bc056d917f07fd77a278b9
Ahssan23 <ahssan180sx@gmail.com>
2025-12-01 21:16:21 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: "https://recipe-application-fty4.onrender.com/",
+  origin: "https://recipe-application-3.onrender.com",
   credentials: true
 }));
 app.use(express.json());
